108c70a4d78e7e0262bda14bd34541b17a3f06e8
Bismark Escobar <bismark.escobar@local.guma.com.ni>
2019-10-09 09:09:13 -0600

    Ultima version

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/Metas','metas_controller@index');
 
 Route::get('/Usuario','usuario_controller@index');
 
-Route::get('/Reportes','reportes_controller@index');
+Route::get('/Reportes','reportes_controller@index'); 
 
 //RUTAS INVENTARIO
 Route::get('/articulos','inventario_controller@getArticulos');
