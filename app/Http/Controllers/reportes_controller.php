8190398eda119196e196d2467d485f7fd16783b3
Bismark Escobar <bismark.escobar@local.guma.com.ni>
2019-11-25 09:18:52 -0600

    Actualizacion 25-11-2019

--- app/Http/Controllers/reportes_controller.php
+++ app/Http/Controllers/reportes_controller.php
@@ -5,19 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\reportes_model;
+
 class reportes_controller extends Controller
 {
-	public function __construct()
-	 {
-	    $this->middleware('auth');//pagina se carga unicamente cuando se este logeado
-	 }
+    public function __construct()
+     {
+        $this->middleware('auth');//pagina se carga unicamente cuando se este logeado
+     }
 
-	 
+     
     function index(){
+        $clases     = reportes_model::claseTerapeutica();
+        $articulos  = reportes_model::articulos();
+        $clientes   = reportes_model::clientes();
         $data = [
             'name' =>  'GUMA@NET',
-            'page' => 'Ventas',
+            'page' => 'Ventas'
         ];
-        return view('pages.reportes',$data);
+        return view('pages.reportes', compact('data', 'clases', 'articulos', 'clientes'));
+    }
+
+    public function detalleVentas(Request $request) {
+        if($request->isMethod('post')) {
+            $obj = reportes_model::returndetalleVentas($request->input('clase'),$request->input('cliente'),$request->input('articulo'),$request->input('mes'),$request->input('anio'));
+            return response()->json($obj);
+        }
     }
 }
