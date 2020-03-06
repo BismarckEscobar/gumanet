12ef448d6a2aff6e1b1a5e29a1e0f8db8b3a0768
Bismark Escobar <bismark.escobar@local.guma.com.ni>
2020-03-06 15:23:10 -0600

    Actualizacion 06/03/2020

--- app/Http/Controllers/dashboard_controller.php
+++ app/Http/Controllers/dashboard_controller.php
@@ -28,13 +28,13 @@ class dashboard_controller extends Controller {
        return view('pages.dashboard',$data);
    }
 
-   public function agregarDatosASession(){
-    $request = Request();
-    $ApplicationVersion = new \git_version();
-     $company = Company::where('id',$request->session()->get('company_id'))->first();// obtener nombre de empresa mediante el id de empresa
-     $request->session()->put('ApplicationVersion', $ApplicationVersion::get());
-     $request->session()->put('companyName', $company->nombre);// agregar nombre de compañia a session[], para obtenert el nombre al cargar otras pagina 
-   }
+    public function agregarDatosASession(){
+      $request = Request();
+      $ApplicationVersion = new \git_version();
+      $company = Company::where('id',$request->session()->get('company_id'))->first();// obtener nombre de empresa mediante el id de empresa
+      $request->session()->put('ApplicationVersion', $ApplicationVersion::get());
+      $request->session()->put('companyName', $company->nombre);// agregar nombre de compañia a session[], para obtenert el nombre al cargar otras pagina 
+    }
 
     public function getTotalRutaXVentas($mes, $anio){
     $obj = dashboard_model::getTotalRutaXVentas($mes, $anio);
@@ -54,6 +54,13 @@ class dashboard_controller extends Controller {
   public function getValBodegas() {
     $obj = dashboard_model::getValBodegas();
     return response()->json($obj);
+  }
+
+  public function ventaXCategorias(Request $request) {
+    if($request->isMethod('post')) {
+      $obj = dashboard_model::ventaXCategorias($request->input('mes'),$request->input('anio'),$request->input('cate'));
+      return response()->json($obj);
+    }
   }
 
   public function getDataGraficas($mes, $anio) {
