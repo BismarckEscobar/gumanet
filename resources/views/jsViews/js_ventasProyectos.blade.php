c6d8e51176fa29ab235b02901f0f8dc21c95f3c0
Bismark Escobar <bismark.escobar@local.guma.com.ni>
2020-12-18 11:14:46 -0600

    bugs

--- resources/views/jsViews/js_ventasProyectos.blade.php
+++ resources/views/jsViews/js_ventasProyectos.blade.php
@@ -13,8 +13,8 @@ $(document).ready(function() {
 	$("select#cmbMes1").prop("selectedIndex", mes1);
 	$("select#cmbMes2").prop("selectedIndex", mes2);
 
-	$("#lblMesActual").text( $("select#cmbMes2 option:selected").text() );
-	$("#lblMesAntero").text( $("select#cmbMes1 option:selected").text() );
+	$("#lblMesActual").text( $("select#cmbMes1 option:selected").text() );
+	$("#lblMesAntero").text( $("select#cmbMes2 option:selected").text() );
 	
 	data = {
 		'anio1' : anio1,
@@ -27,11 +27,11 @@ $(document).ready(function() {
 })
 
 $("#cmbMes1").change( function( event ) {
-	$('#tblVtsProyectos thead tr:eq(0) th:eq(3)').html($("#cmbMes1 option:selected").text());
+	$('#tblVtsProyectos thead tr:eq(0) th:eq(4)').html($("#cmbMes1 option:selected").text());
 });
 
 $("#cmbMes2").change( function( event ) {
-	$('#tblVtsProyectos thead tr:eq(0) th:eq(2)').html($("#cmbMes2 option:selected").text());
+	$('#tblVtsProyectos thead tr:eq(0) th:eq(5)').html($("#cmbMes2 option:selected").text());
 });
 
 $("#compararMeses").click( function() {
@@ -54,8 +54,8 @@ $("#compararMeses").click( function() {
 })
 
 function loadDataVTS(data) {
-	$('.lblAnioActual').text( data['anio1'] );
-	$('.lblAnioAnteri').text( data['anio2'] );
+	$('.lblAnioActual').text( data['anio2'] );
+	$('.lblAnioAnteri').text( data['anio1'] );
 
 	$('#tblVtsProyectos').DataTable({
 		'ajax':{
@@ -83,29 +83,35 @@ function loadDataVTS(data) {
 			{"data": "nombre" },
 			{"data": "ruta" },
 			{"data": "zona" },
-			{"data": "data.mes2.anioActual", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
-			{"data": "data.mes2.anioAnterior", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
 			{"data": null,
 				render: function(data, type, row) { 
-					var temp = (row.data.mes2.anioAnterior==0)?0:((row.data.mes2.anioActual /  row.data.mes2.anioAnterior)-1)*100;
-					return $.fn.dataTable.render.number(',', '.', 0).display( temp )+'%'
+					var temp = (row.data.mes1.anioActual==0)?0:(( row.data.mes1.anioActual / row.data.mes2.anioActual)-1)*100;
+					return $.fn.dataTable.render.number(',', '.', 2).display( temp )+'%'
 				} 
 			},
-			{"data": "data.mes1.anioActual", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
 			{"data": "data.mes1.anioAnterior", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
+			{"data": "data.mes1.anioActual", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
 			{"data": null,
 				render: function(data, type, row) { 
 					var temp = (row.data.mes1.anioAnterior==0)?0:((row.data.mes1.anioActual /  row.data.mes1.anioAnterior)-1)*100;
-					return $.fn.dataTable.render.number(',', '.', 0).display( temp )+'%'
+					return $.fn.dataTable.render.number(',', '.', 2).display( temp )+'%'
+				} 
+			},
+			{"data": "data.mes2.anioAnterior", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
+			{"data": "data.mes2.anioActual", render: $.fn.dataTable.render.number( ',', '.', 2 ) },
+			{"data": null,
+				render: function(data, type, row) { 
+					var temp = (row.data.mes2.anioAnterior==0)?0:((row.data.mes2.anioActual /  row.data.mes2.anioAnterior)-1)*100;
+					return $.fn.dataTable.render.number(',', '.', 2).display( temp )+'%'
 				} 
 			},
 		],
 		"columnDefs": [
 			{ "visible": false, "targets": 0 },
-			{ "width":"20%","targets":[1] },
-			{ "width":"5%","targets":[4,5,6,7,8,9] },
+			
+			{ "width":"6%","targets":[4,5,6,7,8,9,10] },
 			{ "className": "dt-center", "targets": [ 2 ]},
-			{ "className": "dt-right", "targets": [ 4, 5, 6, 7, 8, 9 ]},
+			{ "className": "dt-right", "targets": [ 4, 5, 6, 7, 8, 9, 10 ]},
 		],
 		rowGroup: {
 			dataSrc: 'groupColumn',
@@ -149,23 +155,26 @@ function loadDataVTS(data) {
 
 				crece01 = ( mes2_02==0 )?0:((mes2_01/mes2_02)-1)*100;
 				crece02 = ( mes1_02==0 )?0:((mes1_01/mes1_02)-1)*100;
+				crece03 = ( mes1_01==0 )?0:((mes1_01/mes2_01)-1)*100;
 
 				mes2_01 = $.fn.dataTable.render.number(',', '.', 2).display( mes2_01 );
 				mes2_02 = $.fn.dataTable.render.number(',', '.', 2).display( mes2_02 );
 				mes1_01 = $.fn.dataTable.render.number(',', '.', 2).display( mes1_01 );
 				mes1_02 = $.fn.dataTable.render.number(',', '.', 2).display( mes1_02 );
 
-				crece01 = $.fn.dataTable.render.number(',', '.', 0).display( crece01 )+'%';
-				crece02 = $.fn.dataTable.render.number(',', '.', 0).display( crece02 )+'%';
+				crece01 = $.fn.dataTable.render.number(',', '.', 2).display( crece01 )+'%';
+				crece02 = $.fn.dataTable.render.number(',', '.', 2).display( crece02 )+'%';
+				crece03 = $.fn.dataTable.render.number(',', '.', 2).display( crece03 )+'%';
 
 				return $('<tr/>')
 				.append( `<td class="table-primary font-weight-bold" colspan="3">Total</td>` )
-				.append( `<td class="dt-right table-primary font-weight-bold">`+mes2_01+`</td>` )
-				.append( `<td class="dt-right table-primary font-weight-bold">`+mes2_02+`</td>` )
-				.append( `<td class="dt-right table-primary font-weight-bold">`+crece01+`</td>` )
-				.append( `<td class="dt-right table-primary font-weight-bold">`+mes1_01+`</td>` )
+				.append( `<td class="dt-right table-primary font-weight-bold">`+crece03+`</td>` )
 				.append( `<td class="dt-right table-primary font-weight-bold">`+mes1_02+`</td>` )
-				.append( `<td class="dt-right table-primary font-weight-bold">`+crece02+`</td>` );
+				.append( `<td class="dt-right table-primary font-weight-bold">`+mes1_01+`</td>` )
+				.append( `<td class="dt-right table-primary font-weight-bold">`+crece02+`</td>` )
+				.append( `<td class="dt-right table-primary font-weight-bold">`+mes2_02+`</td>` )
+				.append( `<td class="dt-right table-primary font-weight-bold">`+mes2_01+`</td>` )
+				.append( `<td class="dt-right table-primary font-weight-bold">`+crece01+`</td>` );
 			}
 		},
 		"footerCallback": function ( row, data, start, end, display ) {
@@ -178,58 +187,58 @@ function loadDataVTS(data) {
 			};
 
 			total01 = api
-			.column( 4 )
+			.column( 5 )
 			.data()
 			.reduce( function (a, b) {
 				return intVal(a) + intVal(b);
 			}, 0 );
 
 			total02 = api
-			.column( 5 )
+			.column( 6 )
 			.data()
 			.reduce( function (a, b) {
 				return intVal(a) + intVal(b);
 			}, 0 );			
 
 			total03 = api
-			.column( 7 )
-			.data()
-			.reduce( function (a, b) {
-				return intVal(a) + intVal(b);
-			}, 0 );
-
-			total04 = api
 			.column( 8 )
 			.data()
 			.reduce( function (a, b) {
 				return intVal(a) + intVal(b);
 			}, 0 );
 
-			crece01_ = ( total02==0 )?0:((total01/total02)-1)*100;
-			crece02_ = ( total04==0 )?0:((total03/total04)-1)*100;
+			total04 = api
+			.column( 9 )
+			.data()
+			.reduce( function (a, b) {
+				return intVal(a) + intVal(b);
+			}, 0 );
 
-			$( api.column( 4 ).footer() ).html(
+			crece01_ = ( total02==0 )?0:((total02/total01)-1)*100;
+			crece02_ = ( total04==0 )?0:((total04/total03)-1)*100;
+
+			$( api.column( 5 ).footer() ).html(
 				numeral(total01).format('0,0.00')
 			);
 
-			$( api.column( 5 ).footer() ).html(
+			$( api.column( 6 ).footer() ).html(
 				numeral(total02).format('0,0.00')
 			);
 
-			$( api.column( 6 ).footer() ).html(
-				numeral(crece01_).format('0,0')+'%'
-			);
-
 			$( api.column( 7 ).footer() ).html(
-				numeral(total03).format('0,0.00')
+				numeral(crece01_).format('0,0.00')+'%'
 			);
 
 			$( api.column( 8 ).footer() ).html(
-				numeral(total04).format('0,0.00')
+				numeral(total03).format('0,0.00')
 			);
 
 			$( api.column( 9 ).footer() ).html(
-				numeral(crece02_).format('0,0')+'%'
+				numeral(total04).format('0,0.00')
+			);
+
+			$( api.column( 10 ).footer() ).html(
+				numeral(crece02_).format('0,0.00')+'%'
 			);
 		}
 	});
